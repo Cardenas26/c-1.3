b5789fbb4acb8e8857c12533e91b75257a79600e
Cardenas26 <118994173+Cardenas26@users.noreply.github.com>
2023-03-24 11:44:56 -0500

    quick code

---
+++ c#1.3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter your name: ");
+string name= Console.ReadLine();
+Console.Write("What is your age? ");
+string age= Console.ReadLine();
+Console.WriteLine("Hello "+ name +" your age is "+age);
+Console.ReadLine();
